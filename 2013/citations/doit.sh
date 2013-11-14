54edace6ac16756b1cb0b1fed5177ac3195c4a51
Gedare Bloom <gedare@rtems.org>
2013-11-14 14:42:47 -0500

    citations: split tasks by year.

--- 2013/citations/doit.sh
+++ 2013/citations/doit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo -n "" > output.csv
-awk '{system("python citations.py -y " $1 " -p " $2 " >> output.csv")}' inputs.txt
+rm *.csv
+awk '{system("python citations.py -y " $1 " -p " $2 " >> " $1 ".csv")}' inputs.txt
 
